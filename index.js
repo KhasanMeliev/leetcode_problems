b605f927757959dbb11c885309e449d74b5e2a68
melievkh <khushnudmeliev@gmail.com>
2023-06-06 15:21:39 +0500

    next

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-async function sleep(millis) {
-  return new Promise((delayresolve) => setTimeout(delayresolve, millis));
-}
-
-console.log(sleep(200));
+var fibGenerator = function (n) {
+  let curr = 0;
+  let prev = 1;
+  let nextTerm;
+  let array = [];
+  if (n < 1) {
+    return 1;
+  } else {
+    for (let i = 1; i <= n; i++) {
+      array.push(curr);
+      nextTerm = curr + prev;
+      curr = prev;
+      prev = nextTerm;
+    }
+  }
+  return array;
+};
+fibGenerator(5);
---
+++ solveForExamples.js
@@ -0,0 +1,13 @@
+{
+  //2621. Sleep
+  // async function sleep(millis) {
+  //   return new Promise((delayresolve) => setTimeout(delayresolve, millis));
+  // }
+  // console.log(sleep(200));
+}
+{
+  // 2703. Return Length of Arguments Passed
+  // var argumentsLength = function(...args) {
+  //     return args.length
+  //    };
+}

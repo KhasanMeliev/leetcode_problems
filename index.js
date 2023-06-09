b1e7ee5f113942db5da21f9ce34e3be251b8a2cf
melievkh <khushnudmeliev@gmail.com>
2023-06-09 11:16:36 +0500

    new problem solve

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var isPalindrome = function (head) {
-  let arr = [];
-  while (head) {
-    arr.push(head.val);
-    head = head.next;
-  }
-  return arr.join("") == arr.reverse().join("");
+var removeElement = function (nums, val) {
+  let arrLength = nums.length;
+  let removed = nums.filter((e) => e !== val);
+  let totalLength = arrLength - removed.length;
+  let adding = "_" * totalLength;
+  return removed.push();
 };
-console.log(isPalindrome([1, 2]));
+console.log(removeElement([3, 2, 2, 3, 5], 2));
--- solveForExamples.js
+++ solveForExamples.js
@@ -62,3 +62,22 @@
   // };
   // console.log(isPalindrome([1, 2]));
 }
+{
+  // 20. Valid Parentheses
+  // var isValid = function (s) {
+  //   let arr = [];
+  //   for (let i = 0; i < s.length; i++) {
+  //     if (s[i] == "{") {
+  //       arr.push("}");
+  //     } else if (s[i] == "[") {
+  //       arr.push("]");
+  //     } else if (s[i] == "(") {
+  //       arr.push(")");
+  //     } else if (arr.pop() !== s[i]) {
+  //       return false;
+  //     }
+  //   }
+  //   return !arr.length;
+  // };
+  // console.log(isValid("(){"));
+}

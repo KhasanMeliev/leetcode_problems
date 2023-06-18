820ee55ba848cfbe2c6da4555cff3c1d1a51f285
KhasanMeliev <khasanmeliev2@gmail.com>
2023-06-18 18:38:00 +0500

    new problem

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var myPow = function (x, n) {
-  return Math.pow(x, n).toFixed(5);
+var intToRoman = function (num) {
+  const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  const thousands = ["", "M", "MM", "MMM"];
+
+  let result =
+    thousands[Math.floor(num / 1000)] +
+    hundreds[Math.floor((num % 1000) / 100)] +
+    tens[Math.floor((num % 100) / 10)] +
+    ones[num % 10];
+
+  return result;
 };
-console.log(myPow(2.0, 10));
+
+console.log(intToRoman(1994));
--- solveForExamples.js
+++ solveForExamples.js
@@ -394,3 +394,46 @@
 //   };
 //   console.log(myPow(2.0, 10));
 // }
+
+{
+  // 27. Remove Element
+  // var removeElement = function (nums, val) {
+  //   let res = 0;
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (nums[i] !== val) {
+  //       nums[res] = nums[i];
+  //       res++;
+  //     }
+  //   }
+  //   return res;
+  // };
+  // console.log(removeElement([3, 2, 2, 3], 3));
+}
+
+{
+  // 12. Integer to Roman
+  // var intToRoman = function (num) {
+  //   const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  //   const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  //   const hundreds = [
+  //     "",
+  //     "C",
+  //     "CC",
+  //     "CCC",
+  //     "CD",
+  //     "D",
+  //     "DC",
+  //     "DCC",
+  //     "DCCC",
+  //     "CM",
+  //   ];
+  //   const thousands = ["", "M", "MM", "MMM"];
+  //   let result =
+  //     thousands[Math.floor(num / 1000)] +
+  //     hundreds[Math.floor((num % 1000) / 100)] +
+  //     tens[Math.floor((num % 100) / 10)] +
+  //     ones[num % 10];
+  //   return result;
+  // };
+  // console.log(intToRoman(1994));
+}

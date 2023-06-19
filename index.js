ed7fe4ddece6fe7eafd71081ce96ba9f51abde84
KhasanMeliev <khasanmeliev2@gmail.com>
2023-06-19 10:48:16 +0500

    new problem solved

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-var findDuplicate = function (nums) {
+var firstMissingPositive = function (nums) {
+  nums = nums.filter((i) => !(i <= 0));
   nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] == nums[i + 1]) {
-      return nums[i];
+  nums = [...new Set(nums)];
+  if (nums.length === 1) {
+    return nums[0] > 1 || nums[0] === 0 || nums[0] < 1 ? 1 : 2;
+  }
+  if (nums.length === 0) {
+    return 1;
+  }
+  let biggest = nums[nums.length - 1];
+  let newArr = [];
+  let isEqual = true;
+  let missing = 0;
+  for (let i = 1; i <= biggest; i++) {
+    newArr.push(i);
+  }
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== newArr[j]) {
+      isEqual = false;
+      missing = j;
+      break;
     }
   }
-  return -1;
+  if (isEqual) {
+    return nums[nums.length - 1] + 1;
+  } else {
+    return missing + 1;
+  }
 };
-console.log(findDuplicate([3, 1, 3, 4, 2]));
+
+console.log(firstMissingPositive([1, 2, 0]));

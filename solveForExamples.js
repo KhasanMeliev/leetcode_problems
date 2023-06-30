ee6e8d655d0e2a6d1f42878c97876212c972a792
KhasanMeliev <khasanmeliev2@gmail.com>
2023-06-30 13:49:29 +0500

    capitalizeTitle problem solved

--- solveForExamples.js
+++ solveForExamples.js
@@ -1262,4 +1262,16 @@
 }
 {
   // 2129. Capitalize the Title
+  // var capitalizeTitle = function (title) {
+  //   let res = title
+  //     .split(" ")
+  //     .map((i) => {
+  //       return i.length <= 2
+  //         ? i.toLowerCase()
+  //         : i[0].toUpperCase() + i.slice(1).toLowerCase();
+  //     })
+  //     .join(" ");
+  //   return res;
+  // };
+  // console.log(capitalizeTitle("First leTTeR of EACH Word"));
 }

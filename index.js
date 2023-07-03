908ffd51d64469f6c946bdea3fb11c74d5dcdc48
KhasanMeliev <khasanmeliev2@gmail.com>
2023-07-03 11:14:04 +0500

    new problem solved

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-var maximumNumberOfStringPairs = function(words) {
-  const len = words.length
-  const s = words.map((w) => w.split("").sort().join(""));
-  return len - new Set(s).size
+var isSumEqual = function (firstWord, secondWord, targetWord) {
+  let nums = {
+    a: 0,
+    b: 1,
+    c: 2,
+    d: 3,
+    e: 4,
+    f: 5,
+    g: 6,
+    h: 7,
+    i: 8,
+    j: 9,
+  };
+  let first = [];
+  let second = [];
+  let third = [];
+  let max = Math.max(firstWord.length, secondWord.length, targetWord.length);
+  for (let i = 0; i < max; i++) {
+    first.push(nums[firstWord[i]]);
+    second.push(nums[secondWord[i]]);
+    third.push(nums[targetWord[i]]);
+  }
+
+  return (
+    parseInt(first.join("")) + parseInt(second.join("")) ===
+    parseInt(third.join(""))
+  );
 };
+console.log(isSumEqual("d", "b", "aaaaae"));
--- solveForExamples.js
+++ solveForExamples.js
@@ -1330,3 +1330,50 @@
   //     return len - new Set(s).size
   // };
 }
+{
+  // 1309. Decrypt String from Alphabet to Integer Mapping
+  // var decrypt = (str) => String.fromCharCode(parseInt(str) + 96);
+  // var freqAlphabets = function (s) {
+  //   let text = "";
+  //   for (let i = 0; i < s.length; i++) {
+  //     if (s[i + 2] === "#") {
+  //       text += decrypt(s[i++] + s[i++]);
+  //     } else {
+  //       text += decrypt(s[i]);
+  //     }
+  //   }
+  //   return text;
+  // };
+  // console.log(freqAlphabets("1326#"));
+}
+{
+  // 1880. Check if Word Equals Summation of Two Words
+  //   var isSumEqual = function (firstWord, secondWord, targetWord) {
+  //     let nums = {
+  //       a: 0,
+  //       b: 1,
+  //       c: 2,
+  //       d: 3,
+  //       e: 4,
+  //       f: 5,
+  //       g: 6,
+  //       h: 7,
+  //       i: 8,
+  //       j: 9,
+  //     };
+  //     let first = [];
+  //     let second = [];
+  //     let third = [];
+  //     let max = Math.max(firstWord.length, secondWord.length, targetWord.length);
+  //     for (let i = 0; i < max; i++) {
+  //       first.push(nums[firstWord[i]]);
+  //       second.push(nums[secondWord[i]]);
+  //       third.push(nums[targetWord[i]]);
+  //     }
+  //     return (
+  //       parseInt(first.join("")) + parseInt(second.join("")) ===
+  //       parseInt(third.join(""))
+  //     );
+  //   };
+  //   console.log(isSumEqual("d", "b", "aaaaae"));
+}

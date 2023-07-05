9159ad2bf6123964dc57e7c5ca0baa08cfab9678
KhasanMeliev <khasanmeliev2@gmail.com>
2023-07-06 00:00:43 +0500

    kth factor problem solved

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
-  
-}
-console.log(reverseString("coding"));
+var kthFactor = function (n, k) {
+  let divisors = [];
+  for (let i = 0; i <= n; i++) {
+    if (n % i == 0) {
+      divisors.push(i);
+    }
+  }
+  return divisors.length >= k ? divisors[k - 1] : -1;
+};
+
+console.log(kthFactor(7, 2));
--- solveForExamples.js
+++ solveForExamples.js
@@ -1497,3 +1497,15 @@
   //   return nums[0] * nums[1] - nums[nums.length - 1] * nums[nums.length - 2];
   // };
 }
+{
+  // 1492. The kth Factor of n
+  // var kthFactor = function (n, k) {
+  //   let divisors = [];
+  //   for (let i = 0; i <= n; i++) {
+  //     if (n % i == 0) {
+  //       divisors.push(i);
+  //     }
+  //   }
+  //   return divisors.length >= k ? divisors[k - 1] : -1;
+  // };
+}

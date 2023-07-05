3cef4e5eed64f9f6c96a0d55f928e0b4dabb2f99
KhasanMeliev <khasanmeliev2@gmail.com>
2023-07-05 17:33:01 +0500

    new

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-var sortColors = function (nums) {
-  let nol = [];
-  let one = [];
-  let two = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] == 0) {
-      nol.push(nums[i]);
-    } else if (nums[i] == 1) {
-      one.push(nums[i]);
-    } else {
-      two.push(nums[i]);
-    }
-  }
-
-  return [nol, one, two].flat();
-};
-console.log(sortColors([2, 0, 2, 1, 1, 0]));
+function reverseString(str) {
+  return str.split("").reverse().join("");
+  
+}
+console.log(reverseString("coding"));

a576ff9ed731339993fda4cb0ab9918009d4831d
KhasanMeliev <khasanmeliev2@gmail.com>
2023-07-13 22:06:17 +0500

    new problem solved

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var increasingTriplet = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < nums[i + 1] && nums[i + 1] < nums[i + 2]) {
-      return true;
-    }
-  }
-  return false;
-};
-console.log(increasingTriplet([20, 100, 10, 12, 5, 13]));
-
+function totalMoney(n) {
+  const k = Math.floor(n / 7);
+  const x = n % 7;
+  return (7 * k * (k + 7)) / 2 + (x * (2 * k + x + 1)) / 2;
+}
+console.log(totalMoney(20));

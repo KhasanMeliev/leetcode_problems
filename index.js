323fe04e5cfe3c7729089ad8ce999a243d460efa
KhasanMeliev <khasanmeliev2@gmail.com>
2023-07-16 23:02:37 +0500

    new problem solved

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var minimumCost = function (cost) {
-  cost = cost.sort((a, b) => b - a);
-  return cost.reduce((sum, curr, ind) => sum + (ind % 3 == 2 ? 0 : curr), 0);
+var sumFourDivisors = function (nums) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let count = 2;
+    let currArr = [1, nums[i]];
+    for (let j = 2; j < nums[i]; j++) {
+      if (nums[i] % j == 0) {
+        count++;
+        currArr.push(j);
+      }
+      if (count > 4) break;
+    }
+    if (count === 4) result.push(...currArr);
+  }
+  return result.reduce((a, b) => a + b, 0);
 };
-console.log(minimumCost([6, 5, 7, 9, 2, 2]));
+
+console.log(sumFourDivisors([21, 4, 7]));
--- solveForExamples.js
+++ solveForExamples.js
@@ -2208,5 +2208,22 @@
   // };
 }
 {
-  // 2144. Minimum Cost of Buying Candies With Discount
+  // 1390. Four Divisors
+  // var sumFourDivisors = function (nums) {
+  //   let result = [];
+  //   for (let i = 0; i < nums.length; i++) {
+  //     let count = 2;
+  //     let currArr = [1, nums[i]];
+  //     for (let j = 2; j < nums[i]; j++) {
+  //       if (nums[i] % j == 0) {
+  //         count++;
+  //         currArr.push(j);
+  //       }
+  //       if (count > 4) break;
+  //     }
+  //     if (count === 4) result.push(...currArr);
+  //   }
+  //   return result.reduce((a, b) => a + b, 0);
+  // };
+  // console.log(sumFourDivisors([21, 4, 7]));
 }

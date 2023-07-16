9f25603e90bf9d441cd4f30b5ed4e503296c1b62
KhasanMeliev <khasanmeliev2@gmail.com>
2023-07-16 21:53:47 +0500

    new problem solved

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var minimumAbsDifference = function (arr) {
   let res = [];
   arr = arr.sort((a, b) => a - b);
-  let min = [];
-  for (let i = 0; i <= arr.length; i++) {
-    min.push(arr[i + 1] - arr[i]);
-    if (arr[i] < arr[i + 1] && arr[i + 1] - arr[i] === Math.min(...min)) {
-      res.push([arr[i], arr[i + 1]]);
+  let min = arr[1] - arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] - arr[i] < min) {
+      min = arr[i + 1] - arr[i];
+      res = [];
     }
+    if (arr[i + 1] - arr[i] == min) res.push([arr[i], arr[i + 1]]);
   }
-  return Math.min(...min);
+  return res;
 };
-console.log(minimumAbsDifference([1, 3, 6, 10, 15]));
+console.log(minimumAbsDifference([3, 8, -10, 23, 19, -4, -14, 27]));
 console.log(minimumAbsDifference([40, 11, 26, 27, -20]));
-// -20,11,26,27,40
--- solveForExamples.js
+++ solveForExamples.js
@@ -2193,4 +2193,17 @@
 }
 {
   // 1200. Minimum Absolute Difference
+  // var minimumAbsDifference = function (arr) {
+  //   let res = [];
+  //   arr = arr.sort((a, b) => a - b);
+  //   let min = arr[1] - arr[0];
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i + 1] - arr[i] < min) {
+  //       min = arr[i + 1] - arr[i];
+  //       res = [];
+  //     }
+  //     if (arr[i + 1] - arr[i] == min) res.push([arr[i], arr[i + 1]]);
+  //   }
+  //   return res;
+  // };
 }

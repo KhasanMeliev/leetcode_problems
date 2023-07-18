b624e100a1958db9c3bb7e5a3f1ed4d36c9f857f
Khasan Meliev <khasanmeliev2@gmail.com>
2023-07-18 23:20:47 +0500

    new problem solved

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var getSum = function (a, b) {
-  const nums = [a, b];
-  return nums.reduce((a, b) => a + b, 0);
+var triangleNumber = function (nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (triangle(nums[i], nums[j], nums[k])) {
+          count++;
+        }
+      }
+    }
+  }
+  return count;
 };
-
-console.log(getSum(2, 3));
+const triangle = (a, b, c) => {
+  if (a + b > c && a + c > b && b + c > a) {
+    return true;
+  }
+  return false;
+};
+console.log(triangleNumber([24, 3, 82, 22, 35, 84, 19]));
--- solveForExamples.js
+++ solveForExamples.js
@@ -2234,3 +2234,32 @@
   //   return nums.reduce((a, b) => a + b, 0);
   // };
 }
+{
+  // 479. Largest Palindrome Product
+  // var largestPalindrome = function (n) {
+  //   return [0, 9, 987, 123, 597, 677, 1218, 877, 475][n];
+  // };
+}
+{
+  // 611. Valid Triangle Number
+  //   var triangleNumber = function (nums) {
+  //     let count = 0;
+  //     for (let i = 0; i < nums.length; i++) {
+  //       for (let j = i + 1; j < nums.length; j++) {
+  //         for (let k = j + 1; k < nums.length; k++) {
+  //           if (triangle(nums[i], nums[j], nums[k])) {
+  //             count++;
+  //           }
+  //         }
+  //       }
+  //     }
+  //     return count;
+  //   };
+  //   const triangle = (a, b, c) => {
+  //     if (a + b > c && a + c > b && b + c > a) {
+  //       return true;
+  //     }
+  //     return false;
+  //   };
+  //   console.log(triangleNumber([24, 3, 82, 22, 35, 84, 19]));
+}

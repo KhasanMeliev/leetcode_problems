246839fc44dc10f367131f99e059b71ceed553f3
Khasan Meliev <khasanmeliev2@gmail.com>
2023-07-24 09:25:14 +0500

    solved

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var firstPalindrome = function (words) {
-      let palindromes = [];
-      for (let i = 0; i < words.length; i++) {
-        if (words[i] === words[i].toString().split("").reverse().join("")) {
-          palindromes.push(words[i]);
-        }
-      }
-      return palindromes.length > 0 ? palindromes[0] : "";
-    };
-    
+  let palindromes = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === words[i].toString().split("").reverse().join("")) {
+      palindromes.push(words[i]);
+    }
+  }
+  return palindromes.length > 0 ? palindromes[0] : "";
+};

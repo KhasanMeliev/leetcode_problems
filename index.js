bd98ffa05224fb172ca31f2c2bd016ce8a4b5c65
Khusan <khushnudmeliev@gmail.com>
2023-07-25 12:44:25 +0500

    new problem solved

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-var findErrorNums = function (nums) {
-  let max = Math.max(...nums);
-  let realNums = [];
-  let res = [];
-  let a = [];
-  for (let i = 1; i <= max; i++) {
-    realNums.push(i);
-  }
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        res.push(nums[i]);
+  var findErrorNums = function (nums) {
+    let max = Math.max(...nums);
+    let realNums = [];
+    let res = [];
+    for (let i = 1; i <= max; i++) {
+      realNums.push(i);
+    }
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) {
+          res.unshift(nums[i]);
+        }
+      }
+      if (!nums.includes(realNums[i])) {
+        res.push(realNums[i]);
       }
     }
-  }
-  for (let i = 0; i < realNums.length; i++) {
-    if (!nums.includes(realNums[i])) {
-      res.push(realNums[i]);
+    if (res.includes(undefined)) {
+      res.pop();
+      res.push(max + 1);
     }
-  }
-  return res;
-};
+    return res;
+  };
 
-console.log(findErrorNums([3, 2, 3, 4, 6, 5]));
+console.log(findErrorNums([1, 5, 3, 2, 2, 7, 6, 4, 8, 9]));
 console.log(findErrorNums([3, 2, 2]));
+console.log(findErrorNums([1, 2, 2, 4]));
 console.log(findErrorNums([1, 1]));
 // [1,2,2,4] = [1,2,3,4]
 // [1,2,2] = [1,2,3]
--- solveForExamples.js
+++ solveForExamples.js
@@ -2389,5 +2389,28 @@
   // };
 }
 {
-  // 2540. Minimum Common Value
+  // 645. Set Mismatch
+  // var findErrorNums = function (nums) {
+  //   let max = Math.max(...nums);
+  //   let realNums = [];
+  //   let res = [];
+  //   for (let i = 1; i <= max; i++) {
+  //     realNums.push(i);
+  //   }
+  //   for (let i = 0; i < nums.length; i++) {
+  //     for (let j = i + 1; j < nums.length; j++) {
+  //       if (nums[i] === nums[j]) {
+  //         res.unshift(nums[i]);
+  //       }
+  //     }
+  //     if (!nums.includes(realNums[i])) {
+  //       res.push(realNums[i]);
+  //     }
+  //   }
+  //   if (res.includes(undefined)) {
+  //     res.pop();
+  //     res.push(max + 1);
+  //   }
+  //   return res;
+  // };
 }

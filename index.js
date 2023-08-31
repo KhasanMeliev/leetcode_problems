868d74c980632f66d0f89284b9394ebbaa1f4fe5
Khasan Meliev <khasanmeliev2@gmail.com>
2023-08-31 15:51:11 +0500

    new problem solved-hard-

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
-var vowelStrings = function (words, left, right) {
-  let vowels = ["a", "e", "i", "o", "u"];
-  let arr = words.slice(left, right + 1);
-  let count = [];
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j] === vowels[i] && arr[i][j] === vowels[i]) {
-        count.push(arr[i]);
-      }
-    }
-  }
-  return count;
-};
-
-console.log(vowelStrings(["hey", "aeo", "mu", "ooo", "artro"], 1, 4));
-var findErrorNums = function (nums) {
-  let max = Math.max(...nums);
-  let realNums = [];
+var maximumGap = function (nums) {
+  if (nums.length < 2) return [];
+  nums.sort((a, b) => a - b);
   let res = [];
-  for (let i = 1; i <= max; i++) {
-    realNums.push(i);
+  for (let i = 0; i < nums.length - 1; i++) {
+    res.push(nums[i + 1] - nums[i]);
   }
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        res.unshift(nums[i]);
-      }
-    }
-    if (!nums.includes(realNums[i])) {
-      res.push(realNums[i]);
-    }
-  }
-  if (res.includes(undefined)) {
-    res.pop();
-    res.push(max + 1);
-  }
-  return res;
+  return res.sort((a, b) => a - b)[res.length - 1];
 };
 
-console.log(findErrorNums([1, 5, 3, 2, 2, 7, 6, 4, 8, 9]));
-console.log(findErrorNums([3, 2, 2]));
-console.log(findErrorNums([1, 2, 2, 4]));
-console.log(findErrorNums([1, 1]));
-// [1,2,2,4] = [1,2,3,4]
-// [1,2,2] = [1,2,3]
-// [2,2] = [2,1]
-// [1,1] = [1,2]
-// [3,2,2] = [2,1]
+console.log(maximumGap([3, 6, 9, 1]));
--- solveForExamples.js
+++ solveForExamples.js
@@ -2333,3 +2333,17 @@
   //   return [...map.keys()] || [];
   // };
 }
+
+{
+  // 164. Maximum Gap
+  // var maximumGap = function (nums) {
+  //   if (nums.length < 2) return [];
+  //   nums.sort((a, b) => a - b);
+  //   let res = [];
+  //   for (let i = 0; i < nums.length - 1; i++) {
+  //     res.push(nums[i + 1] - nums[i]);
+  //   }
+  //   return res.sort((a, b) => a - b)[res.length - 1];
+  // };
+  // console.log(maximumGap([3, 6, 9, 1]));
+}

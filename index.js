638ef3d63d2e7fe436a3a09f951bea904636760e
Khasan Meliev <khasanmeliev2@gmail.com>
2023-12-18 20:14:59 +0500

    find a peak element in array

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const binary_search = (n) => {
-  if (n < 2) {
-    return n;
+function equalizeArray(arr) {
+  let arrObj = {};
+  let maxCount = 0;
+  for (let num of arr) {
+    arrObj[num] = arrObj[num] + 1 || 1;
   }
-  let res = 0;
-  let left = 1;
-  let right = Math.floor(n / 2);
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    let sqr = mid ** 2;
-    if (sqr == n) {
-      return mid;
-    } else if (sqr < n) {
-      left = mid + 1;
-      res = mid;
-    } else {
-      right = mid - 1;
+  for (let value in arrObj) {
+    if (arrObj[value] > maxCount) {
+      maxCount = arrObj[value];
     }
   }
-  return res;
-};
-
-console.log(binary_search(25837283728738));
+  return arr.length - maxCount;
+}
+console.log(equalizeArray([2, 3, 4, 5, 2, 3]));
--- index.py
+++ index.py
@@ -1,6 +1,19 @@
-arr = [1, 2, 2, 3, 4]
+arr = [6, 5, 4, 3, 2, 3, 2]
 
-seen = set()
-res = next(i for i in arr if i in seen or seen.add(i))
 
-print(res)
+def func(arr, left, right):
+    mid = (left + right) // 2
+    if (
+        mid == 0
+        or arr[mid - 1] < arr[mid]
+        and mid == len(arr) - 1
+        or arr[mid] > arr[mid + 1]
+    ):
+        return arr[mid]
+    elif mid > 0 and arr[mid - 1] > arr[mid]:
+        return func(arr, left, right - 1)
+    else:
+        return func(arr, left + 1, right)
+
+
+print(func(arr, 0, len(arr) - 1))
